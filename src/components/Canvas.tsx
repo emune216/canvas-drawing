d32f7824f32f556c1d6ec90f9de3f155833ed6a6
yunsu <than4502@gmail.com>
2021-06-18 00:56:52 +0900

    STYLE: responsive web

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 const Canvas = () => {
   return (
-    <>
+    <CavnasWrapper>
       <span>Canvas</span>
-    </>
+    </CavnasWrapper>
   );
-}
+};
+
+const CavnasWrapper = styled.div`
+  background: green;
+
+  @media only screen and (max-width: 600px)  {
+    width: 100%;
+    height: 80%;
+  }
+  @media only screen and (min-width: 600px)  {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default Canvas;
--- src/components/themes/GlobalStyle.js
+++ src/components/themes/GlobalStyle.js
@@ -5,20 +5,11 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
 
-    @media (max-width: 575.98px) {
-      font-size: 0.5rem;
-    }
-    @media (min-width: 576px) and (max-width: 767.98px) {
-      font-size: 0.75rem;
-    }
-    @media (min-width: 768px) and (max-width: 991.98px) {
+    @media only screen and (max-width: 600px) {
       font-size: 1rem;
     }
-    @media (min-width: 992px) and (max-width: 1199.98px) {
-      font-size: 1.25rem;
-    }
-    @media (min-width: 1200px){
-      font-size: 1.5rem;
+    @media only screen and (min-width: 600px){
+      font-size: 2rem;
     }
   }
 `;
